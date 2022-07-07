221bfa8f4209765af9780cd4d9270cb698fc494a
Mo7medSa3ed <68446047+Mo7medSa3ed@users.noreply.github.com>
2022-07-07 11:07:05 +0200

    .

--- app.py
+++ app.py
@@ -1,29 +1,27 @@
 from googletrans import Translator
 from textblob import TextBlob
-from flask import Flask,request
+from flask import Flask,request,jsonify
 
 app = Flask(__name__)
 
 @app.route('/',methods=["POST"]) 
 def result ():
-    request_data = request.get_json()
-    text = request_data['text']
-    translator = Translator()
-    res = translator.translate(text, dest='en')
-    msg=''
-    blob = TextBlob(res.text)
-    if blob.sentiment.polarity > 0:
-        msg= "Positive Review"
-    elif blob.sentiment.polarity < 0:
-        msg= "Negative Review"
-    else:
-        msg= "Nature Review"
-    return {'msg':msg}    
+    reviews = request.get_json()
+    resultData=[]
+    for review in reviews:
+        text = review
+        msg=''
+        translator = Translator()
+        res = translator.translate(text, dest='en')
+        blob = TextBlob(res.text)
+        if blob.sentiment.polarity > 0:
+            msg= "Positive Review"
+        elif blob.sentiment.polarity < 0:
+            msg= "Negative Review"
+        else:
+            msg= "Nature Review"
+        resultData.append({'text': text , 'msg':msg})    
+    return jsonify(resultData)
 
 if __name__ == '__main__':
     app.run()
-
-
-
-
-
